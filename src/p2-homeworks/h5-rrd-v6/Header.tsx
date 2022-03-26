36c0b9bc9c37afad855dc7d852e3f258303c7541
sssromaz <sssromaz@mail.ru>
2022-03-27 00:25:39 +0300

    HW5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import s from "./header.module.css"
+import pic from "./pages/Heand.jpg"
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={s.header}>
 
+            <NavLink to={PATH.PRE_JUNIOR} className={(p) => p.isActive ? s.active : s.unactive}>Pre-junior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={(p) => p.isActive ? s.active : s.unactive}> Junior</NavLink>
+            <NavLink to={PATH.JUNIORPLUS} className={(p) => p.isActive ? s.active : s.unactive}> Junior+</NavLink>
+            <div className={s.slider}><img src={pic}/></div>
         </div>
     )
 }
